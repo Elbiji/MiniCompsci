a06cedc76b6def4f386fbb2fa364a6f5773bec3e
Bagas Noor <19623186@std.stei.itb.ac.id>
2025-10-04 12:58:57 +0700

    feat: added functionality and little interactions for python visualization module

---
+++ public/interface/pythonvizlibraries.js
@@ -0,0 +1,32 @@
+import { loginButton } from "./navbarLoginButton.js";
+import { requireAuth } from "../util/auth.js";
+
+function sectionContainer() {
+    document.querySelectorAll('p[data-target').forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault()
+            const targetId = link.dataset.target
+            const targetElement = document.getElementById(targetId)
+            const yOffest = -100
+            const y = targetElement.getBoundingClientRect().top + window.scrollY + yOffest
+            window.scrollTo({top: y, behavior: 'smooth'})
+        })
+    })   
+}
+
+function handleRedirectQuiz(){
+    const quizBtn = document.getElementById("quiz-btn")
+    quizBtn.addEventListener('click', () => {
+        window.location.href = "./quiz.html"
+    })
+}
+
+function handleRedirectModule(){
+    const moduleBtn = document.getElementById("home-btn")
+    moduleBtn.addEventListener('click', () => {
+        window.location.href = "./module.html"
+    })
+}
+
+
+document.addEventListener('DOMContentLoaded', requireAuth, loginButton, sectionContainer(), handleRedirectQuiz(), handleRedirectModule())
